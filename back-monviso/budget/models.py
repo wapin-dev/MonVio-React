5b843ee03ec9638a8c0af9fd2e8eab39e888a26e
devilghostg <mister.devil6@gmail.com>
2025-08-22 15:01:01 +0400

    feat: initialize budget app with PostgreSQL configuration and basic home view

---
+++ back-monviso/budget/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class Tache(models.Model):
+#     titre = models.CharField(max_length=200)
+#     description = models.TextField(blank=True, null=True)
+#     date_creation = models.DateTimeField(auto_now_add=True)
+#     est_complete = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.titre
--- back-monviso/monviso/settings.py
+++ back-monviso/monviso/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'budget',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'monviso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
